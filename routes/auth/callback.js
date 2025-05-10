c4715c940fe2b7ab15f48069efdb88cfc99379da
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-05-10 11:56:25 +0000

    Revert "Update callback.js w.r.t userInfo"
    This reverts commit 4dac5bcb3f7de735fa90d047f72c980aad6d3f95.

--- routes/auth/callback.js
+++ routes/auth/callback.js
@@ -79,12 +79,8 @@ router.get('/', async (req, res) => {
         });
 
         console.log('Passing userInfo');
-
-        const userProfileText = await userProfile.text();
-
-        console.log('User text', userProfileText);
         
-        const userProfileJson = JSON.parse(userProfileText);
+        const userProfileJson = await userProfile.json();
 
         console.log('Passed UserInfo');
 
