3ed86528cfe999a11f8423f9134fe84b79c2be5a
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-05-12 10:20:42 +0000

    redirect url changed by adding env

--- routes/auth/callback.js
+++ routes/auth/callback.js
@@ -99,7 +99,7 @@ router.get('/', async (req, res) => {
             <html>
                 <head>
                 <script>
-                    window.location.href = '${req.headers.origin}/profile';
+                    window.location.href = '${process.env.FRONTEND_URL}/profile';
                 </script>
                 </head>
                 <body>Redirecting to your dashboard...</body>
